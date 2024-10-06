2f043eb9e9cb7640ea79413cb7c71e79efd995f7
haygcao <ngokelcho@outlook.com>
2024-10-06 22:20:57 +0800

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -34,7 +34,8 @@ const pluginInfo = {
       const response = await fetch(targetUrl, {
         headers: {
           "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+          "Origin": "https://www.so.com" // 手动设置 Origin 头
         }
       });
       if (response.ok) {
