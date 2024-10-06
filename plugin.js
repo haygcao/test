a072adf5bc6173e39135ccb0c89aa607cae79b1b
haygcao <ngokelcho@outlook.com>
2024-10-06 21:37:15 +0800

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -31,7 +31,9 @@ const pluginInfo = {
         headers: {
           "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
           "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36"
-        }
+        },
+        mode: 'no-cors'  // 添加 no-cors 模式
+        
       });
       if (response.ok) {
         const text = await response.text();
