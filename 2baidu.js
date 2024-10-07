fa559465b62bddd3d2832e578ad27daa7e2b41ca
haygcao <ngokelcho@outlook.com>
2024-10-07 10:29:47 +0800

    Update 2baidu.js

--- 2baidu.js
+++ 2baidu.js
@@ -4,7 +4,7 @@ const pluginInfo = {
   info: {
     id: 'your-plugin-id',
     name: 'Your Plugin Name',
-    version: '1.9.75',
+    version: '1.9.76',
     description: 'This is a plugin template.',
     author: 'Your Name',
   },
