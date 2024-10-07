00df5a902b48477832fa4929a57978fbb512972c
haygcao <ngokelcho@outlook.com>
2024-10-07 09:20:45 +0800

    Update newplugin.js

--- newplugin.js
+++ newplugin.js
@@ -4,7 +4,7 @@ const pluginInfo = {
   info: {
     id: 'your-plugin-id',
     name: 'Your Plugin Name',
-    version: '1.9.13',
+    version: '1.9.15',
     description: 'This is a plugin template.',
     author: 'Your Name',
   },
