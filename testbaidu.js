f6671bad31188f1280a029d2beb8e20e61debf28
haygcao <ngokelcho@outlook.com>
2025-01-26 12:58:24 +0800

    Update testbaidu.js

--- testbaidu.js
+++ testbaidu.js
@@ -23,7 +23,7 @@ function queryPhoneInfo(phoneNumber, requestId) {
     pluginId: pluginId,
     method: 'GET',
     requestId: requestId,
-    url: `https://wwww.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&ch=&tn=baiduerr&bar=&wd=${phoneNumber}`,
+    url: `https://wwww.baidu.com/s?wd=${phoneNumber}`,
     headers: {
       "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36',
     },
