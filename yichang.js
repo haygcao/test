7c03af0c8000fc4118857b2820c1639e684ab813
haygcao <ngokelcho@outlook.com>
2025-01-27 23:48:41 +0800

    Update yichang.js

--- yichang.js
+++ yichang.js
@@ -336,11 +336,11 @@ async function generateOutput(phoneNumber, nationalNumber, e164Number) {
     
     // 通过 window.parent.postMessage 发送结果
   console.log(`Plugin ${pluginId} - Sending result via window.parent.PluginResultChannel`);
-  window.parent.PluginResultChannel.postMessage(JSON.stringify({
-    type: 'pluginResult',
-    pluginId: pluginId,
-    data: finalResult,
-  }));
+    window.parent.postMessage(JSON.stringify({
+      type: 'pluginResult',
+      pluginId: pluginId,
+      data: finalResult,
+    }), '*');
 
     
     return finalResult; // 同时返回结果
