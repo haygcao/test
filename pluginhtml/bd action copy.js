b9c8aae5d51c590f346235a6203bf7d09eaf5290
haygcao <29725483+haygcao@users.noreply.github.com>
2026-01-27 21:55:51 +0800

    Update bd action copy.js

--- pluginhtml/bd action copy.js
+++ pluginhtml/bd action copy.js
@@ -63,7 +63,7 @@
 
         const targetUrl = `https://www.baidu.com/s?wd=${encodeURIComponent(phoneNumber)}&ie=utf-8`;
         const headers = { 
-            'User-Agent': config.userAgent || 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36'
+            'User-Agent': config.userAgent || 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36'
         };
 
         sendMessage('httpFetch', JSON.stringify({
@@ -206,4 +206,5 @@
     }
 
     initialize();
+
 })();
